7702cfd8ac40272aa134631a4e4a0fdc833db3ad
Susantakumarb24 <susantakumarb24@gmail.com>
2024-03-08 19:10:58 +0530

    first commit

---
+++ Day1.js
@@ -0,0 +1,24 @@
+//console.log(null == 0);
+/*let x={
+    student:25,
+    name:"Abc"
+}
+console.log("Manasa is "+x.student);
+let y=x;
+x.student++;
+console.log(y.student);
+let x=-2-2;
+let y='2';
+console.log(x==y);
+console.log(x===y);
+console.log(x);
+let arr=[1,2,3,"a","b","c"];
+console.log(arr);
+console.log([...arr]);*/
+let HW=console.log("hau" && true);
+console.log(HW);
+
+
+
+
+
